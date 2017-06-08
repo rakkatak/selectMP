6c0564b614a33a2247b3479585b298bff229aa03
rakkatak <anita@rakkatak.com>
2017-06-08 10:28:06 -0400

    adding ajax request

--- js/controllers.js
+++ js/controllers.js
@@ -21,33 +21,38 @@ findMPControllers.controller('findMPCtrl', ['$scope', '$http', '$filter', functi
       var emailBody = jQuery('#emailBody').val();
       var xm;
 
-      var data = {
-      'message': {
-        'html': emailBody,
-        'text': emailBody,
-        'subject': user.subject,
-        'from_email': user.email,
-        'from_name': user.name,
-        'to': [
-          {
-            name: mpName,
-            email: mpEmail,
-            'type': 'to'
-          }
-        ],
-        'headers': {
-          'Reply-To': 'anita@rakkatak.com'
-        }
-      }
-    };
+      jQuery.ajax({
+  		type: "POST",
+  		url: "https://mandrillapp.com/api/1.0/messages/send.json",
+  		data: {
+   		 'key': '-VDS6QsLxHBa18rXW8VHIg',
+    		'message': {
+    		  'from_email': user.email,
+    		  'to': [
+    		      {
+     		       'email': mpEmail,
+     		       'name': mpName,
+    		        'type': 'to'
+    		      }
+   		     ],
+    		  'autotext': 'true',
+   		   'subject': user.subject,
+   		   'html': emailBody
+   		 }
+  		}
+ 		}).done(function(response) {
+		   console.log(response); // if you're into that sorta thing
+		   console.log(response[0].status); // if you're into that sorta thing
 
-    Mandrill.messages.send(data).success(function(response){
-		xm="Email has been sent";
-		jQuery('#txt').html(xm);
-    }).error(function(response){
-		xm="Error sending email. status ["+response[0].status+"], reject_reason ["+response[0].reject_reason+"]" ;
-		jQuery('#txt').html(xm);
-    });
+           if (response[0].status=="rejected") {
+               xm="Error sending email. status ["+response[0].status+"], reject_reason ["+response[0].reject_reason+"]" ;
+                jQuery('#txt').html(xm);
+           } else {
+               xm="Email has been sent";
+                jQuery('#txt').html(xm);
+           }
+		});
+
   };
 
   $scope.reset = function() {
